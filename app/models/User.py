6f25e35bb49998ef62b7277bf2802d802cad1eb2
Luke Morgan <morgan.luke94@gmail.com>
2020-12-11 23:38:21 +0000

    implements password hasing using bcrypt for login and register fn

--- app/models/User.py
+++ app/models/User.py
@@ -1,13 +1,15 @@
 import html
+import scraps.auth as auth
 from scraps.Db import Database
 
 
-class User():
-    def __init__(self, data: list):
-        self.email = ('user_email', html.escape(data['user-email']))
-        self.password = ('user_password', html.escape(data['user-password']))
+class User:
+    def __init__(self, user_data: dict):
+        self.credentials = {
+            'user_email': html.escape(user_data['user-email']),
+            'user_password': html.escape(user_data['user-password'])
+        }
         self.is_logged_in = False
-
         self.db = Database('scraps_local')
         self.table = 'users'
         self.make_model()
@@ -17,29 +19,50 @@ class User():
             "CREATE TABLE IF NOT EXISTS {table} (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, user_email text, user_password text, created_at DATETIME DEFAULT CURRENT_TIMESTAMP)".format(table=self.table))
         self.db.conn.commit()
 
+    def get_user_email(self):
+        return self.credentials['user_email']
+
+    def get_user_password(self):
+        return self.credentials['user_password']
+
+    def register_user(self):
+        hased_pw = auth.hash_password(self.credentials['user_password'])
+        self.credentials['user_password'] = hased_pw
+        try:
+            self.db.insert_single(self.table, self.credentials)
+            return True
+        except:
+            return False
+
     def is_registered_user(self):
-        (user_column, username) = self.email
-        user_row = self.db.fetch_single(self.table, user_column, username)
+        user_columns = []
+        user_values = []
+        for key, value in self.credentials.items():
+            user_columns.append(key)
+            user_values.append(value)
+
+        user_row = self.db.fetch_single(
+            self.table, user_columns[0], user_values[0])
 
         if not user_row:
             return False
         else:
             return True
 
-    def register_user(self):
-        registrant = [self.email, self.password]
-        try:
-            self.db.insert_single(self.table, registrant)
-            return True
-        except:
-            return False
-
     def log_in_user(self):
-        (email_column_key, username) = self.email
-        (password_column_key, password) = self.password
-        user_row = self.db.fetch_single(self.table, email_column_key, username)
+        user_columns = []
+        user_values = []
+        for key, value in self.credentials.items():
+            user_columns.append(key)
+            user_values.append(value)
 
-        if user_row and username == user_row[1] and password == user_row[2]:
+        user_row = self.db.fetch_single(
+            self.table, user_columns[0], user_values[0])
+
+        is_user_password = auth.check_hashed_password(
+            self.credentials['user_password'], user_row[2])
+
+        if user_row and self.credentials['user_email'] == user_row[1] and is_user_password:
             self.is_logged_in = True
             self.id = user_row[0]
             self.join_date = user_row[3]
@@ -47,16 +70,9 @@ class User():
         else:
             return False
 
-    def get_user_email(self):
-        return self.email
-
-    def get_user_password(self):
-        return self.password
-
     def update_user(self, id: int):
         try:
-            user = [self.email, self.password]
-            self.db.update_single(self.table, user, id)
+            self.db.update_single(self.table, self.credentials, id)
             return True
         except:
             return False
